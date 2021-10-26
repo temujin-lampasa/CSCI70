ce736372aa20cd6030714acf2b5a3cb40ceb1a94
temujin <43642128+temujin-lampasa@users.noreply.github.com>
2021-10-26 13:44:54 +0800

    Add simple hand models

---
+++ simple_hand.py
@@ -0,0 +1,24 @@
+"""
+Simplified version of blackjack.
+Based on https://github.com/SICPDistilled/blackjack
+"""
+from random import randint
+from typing import List
+from functools import reduce
+from operator import add
+
+
+def deal()->int:
+    return randint(1, 10)
+
+def new_hand()->List[int]:
+    return [deal()]
+
+def up_card(hand):
+    return hand[0]
+
+def add_card(hand, card):
+    hand.append(card)
+
+def total(hand):
+    return reduce(add, hand)
