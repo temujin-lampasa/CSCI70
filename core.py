ef7c33ac20bbc6a61d8693a316101671b434f516
temujin <43642128+temujin-lampasa@users.noreply.github.com>
2021-10-26 16:01:40 +0800

    Update core.py
    - Complete function stop_at_17
    - Fix bugs with play_game and play_hand

--- core.py
+++ core.py
@@ -2,7 +2,7 @@ from typing import List, Callable
 from simple_hand import (deal, new_hand, up_card, add_card, total)
 
 def stop_at_17(hand: List[int], opponent_up_card: int) -> bool:
-    pass
+    return total(hand) < 17
 
 def test_strategies(player_strategy: Callable,
                     house_strategy: Callable,
@@ -13,14 +13,13 @@ def play_hand(strategy: Callable, hand: List[int], opponent_up_card: int) -> Lis
     if total(hand) > 21:
         return hand
     if strategy(hand, opponent_up_card):  # Should I hit?
-        play_hand(strategy, add_card(hand, deal()), opponent_up_card)
+        return play_hand(strategy, add_card(hand, deal()), opponent_up_card)
     else:
         return hand
 
-def play_game(player_strategy: function, house_strategy: function) -> int:
+def play_game(player_strategy: Callable, house_strategy: Callable) -> int:
     house_initial_hand = new_hand()
     player_hand = play_hand(player_strategy, new_hand(), up_card(house_initial_hand))
-
     if total(player_hand) > 21:
         return 0
     
