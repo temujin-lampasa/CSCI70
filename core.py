2107e0e000936d040b88dd7b2b08f50bae337fe4
temujin <43642128+temujin-lampasa@users.noreply.github.com>
2021-10-26 15:55:43 +0800

    Add core game functions

---
+++ core.py
@@ -0,0 +1,35 @@
+from typing import List, Callable
+from simple_hand import (deal, new_hand, up_card, add_card, total)
+
+def stop_at_17(hand: List[int], opponent_up_card: int) -> bool:
+    pass
+
+def test_strategies(player_strategy: Callable,
+                    house_strategy: Callable,
+                    num_games: int) -> float:
+    pass
+
+def play_hand(strategy: Callable, hand: List[int], opponent_up_card: int) -> List[int]:
+    if total(hand) > 21:
+        return hand
+    if strategy(hand, opponent_up_card):  # Should I hit?
+        play_hand(strategy, add_card(hand, deal()), opponent_up_card)
+    else:
+        return hand
+
+def play_game(player_strategy: function, house_strategy: function) -> int:
+    house_initial_hand = new_hand()
+    player_hand = play_hand(player_strategy, new_hand(), up_card(house_initial_hand))
+
+    if total(player_hand) > 21:
+        return 0
+    
+    house_hand = play_hand(house_strategy, house_initial_hand, up_card(player_hand))
+
+    if total(house_hand) > 21:
+        return 1   # House bust
+    
+    if total(player_hand) > total(house_hand):
+        return 1   # House lost
+    else:
+        return 0   # Player lost
